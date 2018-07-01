314f0520435575c882e496b68b44db989168c471
Бажен Левковець <bazhen.paseka@gmail.com>
2018-07-01 12:27:40 +0300

    pin and Port now is struck

--- tm1637_sm.c
+++ tm1637_sm.c
@@ -1,22 +1,22 @@
 #include "stm32f1xx_hal.h"
 #include "tm1637_sm.h"
 
-void _tm1637Start(void);
-void _tm1637Stop(void);
-void _tm1637ReadResult(void);
-void _tm1637WriteByte(unsigned char b);
+void _tm1637Start(tm1637_struct tm1637_handler);
+void _tm1637Stop(tm1637_struct tm1637_handler);
+void _tm1637ReadResult(tm1637_struct tm1637_handler);
+void _tm1637WriteByte(tm1637_struct tm1637_handler, unsigned char b);
 void _tm1637DelayUsec(unsigned int i);
-void _tm1637ClkHigh(void);
-void _tm1637ClkLow(void);
-void _tm1637DioHigh(void);
-void _tm1637DioLow(void);
+void _tm1637ClkHigh(tm1637_struct tm1637_handler);
+void _tm1637ClkLow(tm1637_struct tm1637_handler);
+void _tm1637DioHigh(tm1637_struct tm1637_handler);
+void _tm1637DioLow(tm1637_struct tm1637_handler);
 
 // Configuration.
 
-#define CLK_PORT GPIOB
-#define DIO_PORT GPIOB
-#define CLK_PIN GPIO_PIN_0
-#define DIO_PIN GPIO_PIN_1
+// #define CLK_PORT GPIOB
+// #define DIO_PORT GPIOB
+// #define CLK_PIN GPIO_PIN_0
+// #define DIO_PIN GPIO_PIN_1
 #define CLK_PORT_CLK_ENABLE __HAL_RCC_GPIOB_CLK_ENABLE
 #define DIO_PORT_CLK_ENABLE __HAL_RCC_GPIOB_CLK_ENABLE
 
@@ -27,7 +27,7 @@ const char segmentMap[] = {
 };
 
 
-void tm1637Init(void)
+void tm1637Init(tm1637_struct *tm1637_handler)
 {
 	CLK_PORT_CLK_ENABLE();
 	DIO_PORT_CLK_ENABLE();
@@ -35,10 +35,10 @@ void tm1637Init(void)
 	g.Pull = GPIO_PULLUP;
 	g.Mode = GPIO_MODE_OUTPUT_PP; // PP = push pull
 	g.Speed = GPIO_SPEED_FREQ_LOW;
-	g.Pin = CLK_PIN;
-	HAL_GPIO_Init(CLK_PORT, &g);
-	g.Pin = DIO_PIN;
-	HAL_GPIO_Init(DIO_PORT, &g);
+	g.Pin = tm1637_handler->clk_pin;
+	HAL_GPIO_Init(tm1637_handler->clk_port, &g);
+	g.Pin = tm1637_handler->dio_pin;
+	HAL_GPIO_Init(tm1637_handler->dio_port, &g);
 
 	/*
 	  GPIO_InitTypeDef GPIO_InitStruct;
@@ -59,7 +59,7 @@ void tm1637Init(void)
 	*/
 }
 
-void tm1637DisplayDecimal(int v, int displaySeparator)
+void tm1637DisplayDecimal(tm1637_struct *tm1637_handler, int v, int displaySeparator)
 {
     unsigned char digitArr[4];
     for (int i = 0; i < 4; ++i) {
@@ -70,80 +70,80 @@ void tm1637DisplayDecimal(int v, int displaySeparator)
         v /= 10;
     }
 
-    _tm1637Start();
-    _tm1637WriteByte(0x40);
-    _tm1637ReadResult();
-    _tm1637Stop();
+    _tm1637Start(*tm1637_handler);
+    _tm1637WriteByte(*tm1637_handler,0x40);
+    _tm1637ReadResult(*tm1637_handler);
+    _tm1637Stop(*tm1637_handler);
 
-    _tm1637Start();
-    _tm1637WriteByte(0xc0);
-    _tm1637ReadResult();
+    _tm1637Start(*tm1637_handler);
+    _tm1637WriteByte(*tm1637_handler, 0xc0);
+    _tm1637ReadResult(*tm1637_handler);
 
     for (int i = 0; i < 4; ++i) {
-        _tm1637WriteByte(digitArr[3 - i]);
-        _tm1637ReadResult();
+        _tm1637WriteByte(*tm1637_handler, digitArr[3 - i]);
+        _tm1637ReadResult(*tm1637_handler);
     }
 
-    _tm1637Stop();
+    _tm1637Stop(*tm1637_handler);
 }
 
 // Valid brightness values: 0 - 8.
 // 0 = display off.
-void tm1637SetBrightness(char brightness)
+void tm1637SetBrightness(tm1637_struct *tm1637_handler, char brightness)
 {
     // Brightness command:
     // 1000 0XXX = display off
     // 1000 1BBB = display on, brightness 0-7
     // X = don't care
     // B = brightness
-    _tm1637Start();
-    _tm1637WriteByte(0x87 + brightness);
-    _tm1637ReadResult();
-    _tm1637Stop();
+    _tm1637Start(*tm1637_handler);
+    _tm1637WriteByte(*tm1637_handler, 0x87 + brightness);
+    _tm1637ReadResult(*tm1637_handler);
+    _tm1637Stop(*tm1637_handler);
 }
 
-void _tm1637Start(void)
+void _tm1637Start(tm1637_struct tm1637_handler)
 {
-    _tm1637ClkHigh();
-    _tm1637DioHigh();
+    _tm1637ClkHigh(tm1637_handler);
+    _tm1637DioHigh(tm1637_handler);
     _tm1637DelayUsec(2);
-    _tm1637DioLow();
+    _tm1637DioLow(tm1637_handler);
 }
 
-void _tm1637Stop(void)
+void _tm1637Stop(tm1637_struct tm1637_handler)
 {
-    _tm1637ClkLow();
+    _tm1637ClkLow(tm1637_handler);
     _tm1637DelayUsec(2);
-    _tm1637DioLow();
+    _tm1637DioLow(tm1637_handler);
     _tm1637DelayUsec(2);
-    _tm1637ClkHigh();
+    _tm1637ClkHigh(tm1637_handler);
     _tm1637DelayUsec(2);
-    _tm1637DioHigh();
+    _tm1637DioHigh(tm1637_handler);
 }
 
-void _tm1637ReadResult(void)
+void _tm1637ReadResult(tm1637_struct tm1637_handler)
 {
-    _tm1637ClkLow();
+    _tm1637ClkLow(tm1637_handler);
     _tm1637DelayUsec(5);
     // while (dio); // We're cheating here and not actually reading back the response.
-    _tm1637ClkHigh();
+    _tm1637ClkHigh(tm1637_handler);
     _tm1637DelayUsec(2);
-    _tm1637ClkLow();
+    _tm1637ClkLow(tm1637_handler);
 }
 
-void _tm1637WriteByte(unsigned char b)
+void _tm1637WriteByte(tm1637_struct tm1637_handler, unsigned char b)
 {
     for (int i = 0; i < 8; ++i) {
-        _tm1637ClkLow();
+        _tm1637ClkLow(tm1637_handler);
         if (b & 0x01) {
-            _tm1637DioHigh();
+            _tm1637DioHigh(tm1637_handler);
         }
         else {
-            _tm1637DioLow();
+            _tm1637DioLow(tm1637_handler);
         }
         _tm1637DelayUsec(3);
         b >>= 1;
-        _tm1637ClkHigh();
+        _tm1637ClkHigh(tm1637_handler);
         _tm1637DelayUsec(3);
     }
 }
@@ -157,22 +157,22 @@ void _tm1637DelayUsec(unsigned int i)
     }
 }
 
-void _tm1637ClkHigh(void)
+void _tm1637ClkHigh(tm1637_struct tm1637_handler)
 {
-    HAL_GPIO_WritePin(CLK_PORT, CLK_PIN, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(tm1637_handler.clk_port, tm1637_handler.clk_pin , GPIO_PIN_SET);
 }
 
-void _tm1637ClkLow(void)
+void _tm1637ClkLow(tm1637_struct tm1637_handler)
 {
-    HAL_GPIO_WritePin(CLK_PORT, CLK_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(tm1637_handler.clk_port, tm1637_handler.clk_pin, GPIO_PIN_RESET);
 }
 
-void _tm1637DioHigh(void)
+void _tm1637DioHigh(tm1637_struct tm1637_handler)
 {
-    HAL_GPIO_WritePin(DIO_PORT, DIO_PIN, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(tm1637_handler.dio_port, tm1637_handler.dio_pin, GPIO_PIN_SET);
 }
 
-void _tm1637DioLow(void)
+void _tm1637DioLow(tm1637_struct tm1637_handler)
 {
-    HAL_GPIO_WritePin(DIO_PORT, DIO_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(tm1637_handler.dio_port, tm1637_handler.dio_pin, GPIO_PIN_RESET);
 }
