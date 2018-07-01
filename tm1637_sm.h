3832aa6e44edbee21515eda2336f25b14f031e3c
Бажен Левковець <bazhen.paseka@gmail.com>
2018-07-01 15:06:20 +0300

    move "__HAL_RCC_GPIOB_CLK_ENABLE()" to main

--- tm1637_sm.h
+++ tm1637_sm.h
@@ -12,18 +12,18 @@ typedef struct
 
 typedef enum
 {
-	bright_off,
-	bright_15percent,
-	bright_30percent,
-	bright_45percent,
-	bright_60percent,
-	bright_75percent,
-	bright_90percent,
-	bright_full,
+	bright_off			= 0,
+	bright_15percent	= 1,
+	bright_30percent	= 2,
+	bright_45percent	= 3,
+	bright_60percent	= 4,
+	bright_75percent	= 5,
+	bright_90percent	= 6,
+	bright_full			= 7,
 } brightness_enum;
 
 void tm1637_Init(tm1637_struct *tm1637_handler);
-void tm1637_Display_Decimal(tm1637_struct *tm1637_handler, uint32_t value, uint8_t displaySeparator);
+void tm1637_Display_Decimal(tm1637_struct *tm1637_handler, uint32_t value, uint8_t DisplaySeparator);
 void tm1637_Set_Brightness(tm1637_struct *tm1637_handler, brightness_enum brightness);
 
 #endif // TM1637_SM_H_INCLUDED
